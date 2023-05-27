874eacb8f01a717f36632d2501dbc0ba47f4b74e
jiwon <kiwijomn@gmail.com>
2023-05-28 01:21:20 +0900

    update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ec2-user
-PROJECT=danggeun
 
 cd $REPOSITORY
 
-JAR_NAME=$(ls $REPOSITORY/$PROJECT/  | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/$PROJECT/$JAR_NAME
+JAR_NAME=$(ls $REPOSITORY/  | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/$JAR_NAME
 CURRENT_PID=$(pgrep -fl $JAR_PATH | awk '{print $1}')
 
 if [ -z $CURRENT_PID ]
@@ -21,5 +20,5 @@ fi
 echo "> $JAR_PATH Run"
 
 nohup java -jar \
-  -Dspring.config.location=file:///$REPOSITORY/$PROJECT/application.yml \
-  $JAR_PATH >> /$REPOSITORY/$PROJECT/log.out 2>&1 &
+  -Dspring.config.location=file:///$REPOSITORY/application.yml \
+  $JAR_PATH >> /$REPOSITORY/log.out 2>&1 &
